76862698fe830dad8de089107cdc7f819977d2ff
Daniel Laubacher <dlaubacher3@gmail.com>
2017-09-29 14:28:56 -0400

    Initial Commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+<main>
+<h1>
+<?php 
+
+echo "Hello from PHP";
+
+?>
+</h1>
+</main>
+
+</body>
+</html>
