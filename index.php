2b6b5d17d38722f010e1d2581ee6699749b72801
Daniel Laubacher <dlaubacher3@gmail.com>
2017-09-29 15:37:33 -0400

    setup styles and scripts

--- index.php
+++ index.php
@@ -2,11 +2,15 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>PHP Apple Home</title>
+	<link rel="stylesheet" href="assets/styles/style.css">
+
 </head>
 <body>
 
-<main>
+<main class="main">
 <h1>
 <?php 
 
@@ -14,7 +18,10 @@ echo "Hello from PHP";
 
 ?>
 </h1>
-</main>
-
+</main class="main">
+<footer>
+<script src="assets/scripts/main.js" >
+</script>
+</footer>
 </body>
 </html>
