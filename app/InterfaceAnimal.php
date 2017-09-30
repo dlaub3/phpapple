fa803824e0c84bc587a899912d6ed54ae8c44208
Daniel Laubacher <dlaubacher3@gmail.com>
2017-09-30 08:00:30 -0400

    Deleted InterfaceAnimal  Please enter the commit message for your changes. Lines starting

--- app/InterfaceAnimal.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//An interface must use Public method signatures.
-//All methods must be implemented when used.:
-
-
-interface InterfaceKingdom {
-    public static function kingdom($kingdom);
-}
